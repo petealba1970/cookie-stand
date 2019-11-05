abcfcd796e1178ca8c369d189c84eecbb7d64fcc
Pete Alba <petealba1970@gmail.com>
2019-11-04 23:14:39 -0800

    moved functions

--- app.js
+++ app.js
@@ -5,29 +5,16 @@ var Seattle = {
     min: 23,
     max: 65,
     avg: 6.3,
-    customers: function () {
-        this.min = Math.floor(this.min);
-        this.max = Math.ceil(this.max);
-        return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-    },
-    cookiesSoldperHour: function () {
-        return this.customers() * this.avg
-    }
+
 }
+
 var Tokyo = {
 
     name: 'Tokyo',
     min: 3,
     max: 24,
     avg: 1.2,
-    customers: function () {
-        this.min = Math.floor(this.min);
-        this.max = Math.ceil(this.max);
-        return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-    },
-    cookiesSoldperHour: function () {
-        return this.customers() * this.avg
-    }
+
 }
 
 var Dubai = {
@@ -36,14 +23,7 @@ var Dubai = {
     min: 11,
     max: 38,
     avg: 3.7,
-    customers: function () {
-        this.min = Math.floor(this.min);
-        this.max = Math.ceil(this.max);
-        return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-    },
-    cookiesSoldperHour: function () {
-        return this.customers() * this.avg
-    }
+
 }
 var Paris = {
 
@@ -51,14 +31,7 @@ var Paris = {
     min: 20,
     max: 38,
     avg: 2.3,
-    customers: function () {
-        this.min = Math.floor(this.min);
-        this.max = Math.ceil(this.max);
-        return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-    },
-    cookiesSoldperHour: function () {
-        return this.customers() * this.avg
-    }
+
 }
 
 var Lima = {
@@ -67,38 +40,40 @@ var Lima = {
     min: 2,
     max: 16,
     avg: 4.6,
-    customers: function () {
-        this.min = Math.floor(this.min);
-        this.max = Math.ceil(this.max);
-        return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-
-    },
-    cookiesSoldperHour: function () {
-        return this.customers() * this.avg
-    }
 
 }
 var StoreLocationDiv = document.getElementById('StoreLocation')
 var cookiesSoldDul = document.getElementById('Cookiessold')
 
 //Creating arrays to store data for each store location
-
 var storeNow = [Seattle, Tokyo, Dubai, Paris, Lima];
 for (var storeIndex = 0; storeIndex < storeNow.length; storeIndex++) {
     var paragraph = document.createElement('p');
+
     var currentLocation = storeNow[storeIndex];
     paragraph.textContent = currentLocation.name;
     StoreLocationDiv.append(paragraph);
     //loop to go through data of each store per hour in the morning
     var StoreHours = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7]
     for (var EachHour = 0; EachHour < StoreHours.length; EachHour++) {
+        function Customers() {
+            currentLocation.min = Math.floor(currentLocation.min);
+            currentLocation.max = Math.ceil(currentLocation.max);
+            return Math.floor(Math.random() * (currentLocation.max - currentLocation.min + 1)) + currentLocation.min;
+
+        }
+        var customersPer = Customers();
+        // function cookiesSold() {
+        // return customersPer * avg
+
         var Hourstatement = document.createElement('li');
         var currentTime = StoreHours[EachHour];
-        Hourstatement.textContent = currentTime + this.customers + '  cookies.';
+        Hourstatement.textContent = currentTime + '  cookies.';
         StoreLocationDiv.append(Hourstatement);
         console.log(StoreHours[EachHour]);
-        console.log(currentLocation.min);
+        console.log(customersPer);
+
 
     }
-
+    console.log(currentLocation.avg);
 }
